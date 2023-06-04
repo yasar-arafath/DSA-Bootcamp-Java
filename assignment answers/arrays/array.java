194b229b34a964e63838205ddac71fa2da7f118d
Yasar arafat E <yasararafat8426@gmail.com>
2023-06-04 17:11:29 +0530

    arr

--- assignment answers/arrays/array.java
+++ assignment answers/arrays/array.java
@@ -1,9 +1,15 @@
-class array {
-    public int[] buildArray(int[] nums) {
-        int [] ans = new int [nums.length];
-        for(int i =0;i<nums.length;i++){
+public class array {
+    public static int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
             ans[i] = nums[nums[i]];
         }
         return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 0 };
+        int[] result = buildArray(nums);
+        System.out.println("Result: " + Arrays.toString(result));
     }
 }
